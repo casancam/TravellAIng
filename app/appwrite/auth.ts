422d3b104f836537d8bd6fe561ff17816b8d3618
Carlos Sanchez <carlossc999@gmail.com>
2025-08-03 12:52:53 +0100

    route updated, sign-in improved

--- app/appwrite/auth.ts
+++ app/appwrite/auth.ts
@@ -4,7 +4,11 @@ import { redirect } from "react-router";
 
 export const loginWithGoogle = async () => {
   try {
-    account.createOAuth2Session(OAuthProvider.Google);
+    account.createOAuth2Session(
+      OAuthProvider.Google,
+      `${origin}/dashboard`,
+      `${origin}/sign-in`
+    );
   } catch (error) {
     console.log("Error in loginWithGoogle: ", error);
   }
